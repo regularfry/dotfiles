337f08b868c1d9bc56b1fee6745b24daab9bb7cb
Alex Young <alex@bytemark.co.uk>
2013-03-05 10:40:52 +0000

    Add zsh to install.sh.x

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 apt-get install -yy --force-yes emacs vim realpath emacs-goodies-el \
   paredit-el git-core mercurial subversion yaml-mode haskell-mode \
-  erlang-mode sshpass build-essential haskell-platform
+  erlang-mode sshpass build-essential haskell-platform zsh
