486cb745fcd72eef9d753a80fb29d5c05c4080ba
Alex Young <alex@bytemark.co.uk>
2013-03-05 10:09:52 +0000

    Add a package installer

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get install -yy --force-yes emacs vim realpath emacs-goodies-el \
+  paredit-el git-core mercurial subversion yaml-mode haskell-mode \
+  erlang-mode sshpass build-essential
