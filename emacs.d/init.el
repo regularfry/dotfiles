444538ce186a1723925ef1ea137d540fa0079e1b
Alex Young <alex@bytemark.co.uk>
2013-03-05 10:15:39 +0000

    Switched to inconsolata in emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -110,4 +110,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 82 :width normal :foundry "bitstream" :family "Bitstream Vera Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "gray12" :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "Inconsolata")))))
