5d6c102fdb7da7bf68d3ed8e016c75f77373945a
Alex Young <alex@blackkettle.org>
2015-07-19 13:50:54 +0100

    New look init.el, removes yasnippet from plugins and loads from elpa

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,24 +1,29 @@
-(setq indent-line-function 'insert-tab)
-(setq-default transient-mark-mode t)
-(setq-default indent-tabs-mode nil)
-(setq x-select-enable-clipboard t)
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq inhibit-startup-message t)
 (fset 'yes-or-no-p 'y-or-n-p)
-(setq-default tab-width 2)
-(setq js-indent-level 2)
-(show-paren-mode t)
-(tool-bar-mode nil)
-(menu-bar-mode nil)
 (server-start)
-(ido-mode t)
-
-(require 'uniquify)
-(setq
-  uniquify-buffer-name-style 'forward
-  uniquify-separator ":")
-
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(require 'package)
+(add-to-list 'package-archives
+  '("marmalade" .  "https://marmalade-repo.org/packages/"))
+
+(defvar my-packages '(better-defaults
+                      paredit
+                      idle-highlight-mode
+                      ido-ubiquitous
+                      find-file-in-project
+                      magit
+                      smex
+                      scpaste
+                      yaml-mode
+                      yasnippet))
+
+(package-initialize)
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 
 ;; filename matching
 (add-to-list 'auto-mode-alist '("[rR]akefile\\'" . ruby-mode))
@@ -27,97 +32,11 @@
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 
-
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (add-to-list 'write-file-functions
-                         'delete-trailing-whitespace)))
-
-
-(defun make-variable ()
-   (let ((var-name (read-string "Variable name: ")))
-     (kill-append " = " t)
-     (kill-append var-name t)
-     (insert-string var-name)
-     (end-of-line 0)
-     (newline-and-indent)
-     (yank)))
-
-(defun extract-region-to-variable ()
-   (interactive)
-   (kill-region (region-beginning) (region-end))
-   (make-variable))
-
-(defun extract-sexp-to-variable ()
-  (interactive)
-  (kill-sexp)
-  (make-variable))
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-
-(defun haml-extract-partial (name)
-  (interactive "MPartial name: ")
-  (write-region (region-beginning) (region-end)
-                (concat "_" name ".html.haml"))
-  (kill-region (region-beginning) (region-end))
-  (insert "=render :partial => \"" name "\"\n"))
-
-
-
-
-;; set up for slime (obsolete, replace)
-(add-to-list 'load-path "~/.emacs.d/slime")
-(eval-after-load "slime"
-  '(progn (slime-setup '(slime-repl))))
-(require 'slime)
-(slime-setup)
-
-
-(defun lisp-enable-paredit-hook ()
-  (paredit-mode 1))
-(add-hook 'clojure-mode-hook 'lisp-enable-paredit-hook)
-
-
-;; Set up snippets
-(add-to-list 'load-path
-	     "~/.emacs.d/plugins/yasnippet-0.6.1c")
-(require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
-
-
-;; Set up marmalade
-(add-to-list 'load-path
-	     "~/.emacs.d/plugins/package")
-(require 'package)
-(add-to-list 'package-archives
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
-(package-initialize)
-
-
-(defun make-journal-entry ()
-  (interactive)
-  (let ((filename
-         (apply 'format "%d-%d-%d_%d:%d:%d.md"
-                (cdddr (reverse (decode-time))))))
-    (switch-to-buffer
-     (find-file (concat "/ssh:alex@81.187.127.205:journal/" filename)))))
-(global-set-key [f9] 'make-journal-entry)
+(add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
 
 ; C-? reverts the buffer.  Logic is that C-/ is undo, so C-S-/ is
 ; more-undo.
 (global-set-key (kbd "C-?") 'revert-buffer)
-
 
 ; make f12 edit this file
 (defun edit-init-el ()
@@ -144,9 +63,6 @@
 (global-set-key [?\C->] 'increase-left-margin)
 (global-set-key [?\C-<] 'decrease-left-margin)
 
-; Set to hippie-expand by default
-(global-set-key (kbd "M-/") 'hippie-expand)
-
 
 ; Handy binding
 (global-set-key (kbd "C-c a =")
@@ -154,24 +70,88 @@
                   (align-regexp (region-beginning) (region-end) "\\(\\s-*\\)=" 1 1 nil)))
 (global-set-key (kbd "M-=") 'align-regexp)
 
-
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-calm-forest)
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(global-rainbow-delimiters-mode t))
-(custom-set-faces
+(defun set-font (fontname)
+  (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray12" :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "Inconsolata"))))
- '(minimap-font-face ((default (:height 20 :family "DejaVu Sans Mono")) (nil nil))))
+   '(default ((t (:inherit nil :stipple nil :background "gray12"
+                  :foreground "green" :inverse-video nil :box nil :strike-through nil
+                  :overline nil :underline nil :slant normal :weight normal :height
+                  100 :width normal :foundry "unknown" :family fontname))))
+   '(minimap-font-face ((default (:height 20 :family "DejaVu Sans Mono")) (nil nil)))))
 
-(color-theme-initialize)
-(color-theme-solarized-dark)
+(defun set-fixed-font ()  (interactive) (set-frame-font "Inconsolata 12"))
+(defun set-proportional-font ()  (interactive) (set-frame-font "Gentium 12"))
+
+(set-fixed-font)
+;;(set-proportional-font)
+(global-set-key [f9] 'set-fixed-font)
+(global-set-key [f10] 'set-proportional-font)
+
+;;; Some basic navigation customizations.
+
+(setq line-move-visual nil)             ; Default to logical line movement
+
+;;; The following function are like previous-line/next-line, but move
+;;; visually (and do so without changing the default behaviour).
+
+(defun previous-visual-line ()
+  "Move to previous visual line, without effecting the default behavior."
+  (interactive)
+  (let ((line-move-visual t))
+    (previous-line)))
+
+(defun next-visual-line ()
+  "Move to next visual line, without effecting the default behavior."
+  (interactive)
+  (let ((line-move-visual t))
+    (next-line)))
+
+
+(global-set-key (kbd "<down>") 'next-visual-line)
+(global-set-key (kbd "<up>") 'previous-visual-line)
+
+(global-set-key (kbd "C-r") 'isearch-backward)
+(global-set-key (kbd "C-s") 'isearch-forward)
+
+;;; Meta *
+
+(global-set-key (kbd "M-g") 'goto-line)
+
+(defun move-line-region-up (start end n)
+  (interactive "r\np")
+  (if (region-active-p) (move-region-up start end n) (move-line-up n)))
+
+(defun move-line-region-down (start end n)
+  (interactive "r\np")
+  (if (region-active-p) (move-region-down start end n) (move-line-down n)))
+
+(defun move-region (start end n)
+  "Move the current region up or down by N lines."
+  (interactive "r\np")
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun move-region-up (start end n)
+  "Move the current line up by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) -1 (- n))))
+
+(defun move-region-down (start end n)
+  "Move the current line down by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) 1 n)))
+
+(global-set-key (kbd "M-n") 'move-region-down)
+(global-set-key (kbd "M-p") 'move-region-up)
+
+
+(require 'yasnippet)
+(add-hook 'ruby-mode-hook 'yas/minor-mode-on)
+(yas/reload-all)
